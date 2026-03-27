c852730e2f863ed9c23925bbb92a72740f87a517
Gabriela Oliveira <gabiiholiveiraa04@gmail.com>
2026-03-27 10:18:47 -0300

    adicionado novos tratamentos de exceções

---
+++ src/main/java/com/javanauta/usuario/infrastucture/exceptions/UnauthorizedException.java
@@ -0,0 +1,14 @@
+package com.javanauta.usuario.infrastucture.exceptions;
+
+import javax.naming.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable throwable){
+        super(message, throwable);
+    }
+}
